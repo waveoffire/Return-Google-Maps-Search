99504fee4f68760c405b744ea8aa05849c825bb4
waveoffire <waveoffire98@gmail.com>
2024-02-27 12:56:32 +0100

    init

---
+++ scripts/content.js
@@ -0,0 +1,20 @@
+const queryString = window.location.search;
+
+const urlParams = new URLSearchParams(queryString);
+const search = urlParams.get('q')
+console.log(search)
+let elem
+if (search && window.location.host.includes('google')) {
+    let els = document.getElementsByTagName('a');
+    for (var i = 0, l = els.length; i < l; i++) {
+        var el = els[i];
+        if (el.href.includes('tbm=vid')) {
+            elem = el.parentElement
+            break
+        }
+    }
+    let maps_button = elem.cloneNode(true)
+    maps_button.firstChild.href = "https://www.google.com/maps/search/" + search
+    maps_button.firstChild.firstChild.firstChild.innerText = "Maps"
+    elem.after(maps_button)
+}
